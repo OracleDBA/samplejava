2a1f76aa799a94d4e08123f01857b94b91dff26f
Rahil <Rahil@oracle.com>
2020-02-27 10:47:07 +0000

    Webhook trigger Remodified

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ class sample{
 	public static void main(String arg[]){
 	       System.out.println(" Automation using Jenkins");
 	       System.out.println("Code Executed by - Rahil ");
-	       System.out.println("http://13.127.145.74:8080/github-webhook/")"
+	       System.out.println("http://13.127.145.74:8080/github-webhook/");
 	}
 }
 
